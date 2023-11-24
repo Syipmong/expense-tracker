781773830d912f28cadb04e404ec45b969ce9fa1
syipmong <syipmong5@gmail.com>
2023-11-24 23:10:34 +0100

    created the project

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit src/App.js and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Yipmong
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
