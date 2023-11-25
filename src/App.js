0361fff7c0267d980b2d4db34e8a74d3f0983d5a
syipmong <syipmong5@gmail.com>
2023-11-25 15:48:15 +0100

    made changes by adding hover and active effects in the dashboard categories

--- src/App.js
+++ src/App.js
@@ -10,7 +10,6 @@ function App() {
       <NavBar/>
       <Dashboard/>
       <Footer/>
-      
     </div>
   );
 }
--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,27 +1,55 @@
+import './dashboard.css';
 import React from 'react';
-import ExpenseList from './ExpenseList';
-// import Charts from './Charts';
 
 const Dashboard = () => {
-    // Dummy data for expenses
-    const expenses = [
-        { id: 1, description: 'Groceries', amount: 50.00 },
-        { id: 2, description: 'Dinner', amount: 30.00 },
-        { id: 3, description: 'Gas', amount: 20.00 },
-        // Add more expenses as needed
-    ];
-
-    return (
-        <div className="dashboard">
-            <h2>Dashboard</h2>
-
-            {/* Display charts */}
-            {/* <Charts expenses={expenses} /> */}
-
-            {/* Display the list of expenses */}
-            <ExpenseList expenses={expenses} />
+  return (
+    <div className="dashboard">
+      {/* Overview Section */}
+      <section className="overview">
+        <div className="total-expenses">
+          <h3>Total Expenses</h3>
+          {/* Placeholder for total expenses amount */}
         </div>
-    );
+        <div className="monthly-expenses">
+          <h3>Monthly Expenses</h3>
+          {/* Placeholder for monthly expenses amount */}
+        </div>
+        <div className="expense-trends">
+          <h3>Expense Trends</h3>
+          {/* Placeholder for chart */}
+        </div>
+      </section>
+
+     
+      <section className="expense-categories">
+        <div className="category-breakdown">
+          <h3>Expense Breakdown</h3>
+          {/* Placeholder for pie chart */}
+        </div>
+        <div className="top-categories">
+          <h3>Top Spending Categories</h3>
+          {/* Placeholder for top categories list */}
+        </div>
+      </section>
+
+      {/* Recent Transactions */}
+      <section className="recent-transactions">
+        <h2>Recent Transactions</h2>
+        
+      </section>
+
+      
+      <section className="budget-progress">
+        <h2>Budget Progress</h2>
+        
+      </section>
+
+      <section className="quick-actions">
+        <button>Add Expense</button>
+        <button>Filter Transactions</button>
+      </section>
+    </div>
+  );
 }
 
 export default Dashboard;
