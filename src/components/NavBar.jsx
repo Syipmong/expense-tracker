769bb626320d6f523e6894470f74f48b33f1f26d
syipmong <syipmong5@gmail.com>
2023-11-25 03:54:39 +0100

    building the navbar component

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,13 +6,18 @@ const Navbar = () => {
     <nav className="navbar">
       <div className="logo">Expense Tracker</div>
       <ul className="nav-links">
-        <li><a href="https://money.com">Home</a></li>
-        <li><a href="https://money.com">Transactions</a></li>
-        <li><a href="https://money.com">Categories</a></li>
+        <li><a href="#home">Home</a></li>
+        <li><a href="#transactions">Transactions</a></li>
+        <li><a href="#categories">Categories</a></li>
       </ul>
+      <div className="buttons">
+        <button className="primary-button">Add Expense</button>
+        <button className="secondary-button">Log Out</button>
+      </div>
     </nav>
   );
 }
 
 export default Navbar;
+
 
