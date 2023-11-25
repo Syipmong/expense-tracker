285caf900891ac834a58d6d7743cbada6e99f5f5
syipmong <syipmong5@gmail.com>
2023-11-26 00:53:25 +0100

    test run

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Total Expenses/i);
+  const linkElement = screen.getByText(/Total Expenses/);
   expect(linkElement).toBeInTheDocument();
 });
