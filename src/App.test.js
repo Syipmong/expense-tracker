7aa2f5f980e52b4835dc732d76c1473d22c15804
syipmong <syipmong5@gmail.com>
2023-11-25 06:50:24 +0100

    building the basic components

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Hello World/i);
+  const linkElement = screen.getByText(/Dashboard/i);
   expect(linkElement).toBeInTheDocument();
 });
