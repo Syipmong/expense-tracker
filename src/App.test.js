f4a74553d1fd93c97adba0e9606ee718af9b8243
syipmong <syipmong5@gmail.com>
2023-11-26 01:00:43 +0100

    test run

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('expense', () => {
   render(<App />);
   const linkElement = screen.getByText(/Expense/i);
   expect(linkElement).toBeInTheDocument();
