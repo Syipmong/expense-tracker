2696fc9cbd30515c88155c42163762265a7e6d50
syipmong <syipmong5@gmail.com>
2023-12-08 16:20:51 +0100

    added some mobile responsiveness

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,13 +6,13 @@ const Navbar = () => {
     <nav className="navbar">
       <div className="logo">Expense Tracker</div>
       <ul className="nav-links">
-        <li><a href="#home"><i className="fas fa-home"></i>Home</a></li>
-        <li><a href="#transactions"><i className="fas fa-exchange-alt"></i>Transactions</a></li>
-        <li><a href="#categories"><i className="fas fa-list-alt"></i>Categories</a></li>
-        <li><a href='#expenselist'><i className='fas fa-dollar-sign'></i>Expenses</a></li>
+        <li><a href="#home"><i className="fas fa-home"></i><span>Home</span></a></li>
+        <li><a href="#transactions"><i className="fas fa-exchange-alt"></i><span>Transactions</span></a></li>
+        <li><a href="#categories"><i className="fas fa-list-alt"></i><span>Categories</span></a></li>
+        <li><a href='#expenselist'><i className='fas fa-dollar-sign'></i><span>Expenses</span></a></li>
       </ul>
       <div className="buttons">
-        <button className="primary-button" ><i className="fas fa-plus"></i>Add Expense</button>
+        {/* <button className="primary-button" ><i className="fas fa-plus"></i>Add Expense</button> */}
         <button className="secondary-button"><i className="fas fa-sign-out-alt"></i>Log Out</button>
       </div>
     </nav>
