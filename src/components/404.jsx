8dca6afd474edf9723faf475b1325fd6674603cd
syipmong <syipmong5@gmail.com>
2024-01-05 08:40:45 +0100

    Refactor 404 page component
    The commit message suggests that the developer has refactored the 404 page component.

--- src/components/404.jsx
+++ src/components/404.jsx
@@ -8,6 +8,7 @@ const NotFoundPage = () => {
             <p className="not-found-message">Oops! The page you are looking for does not exist.</p>
             <img src="/images/404-image.png" alt="404 Error" className="not-found-image" />
             <a href="/" className="not-found-link">Go back to homepage</a>
+            {/* Images not configured yet */}
         </div>
     );
 };
