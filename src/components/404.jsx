05df6ae7f0f0b87ea6fb4271fd21d408cfee2521
syipmong <syipmong5@gmail.com>
2024-01-11 23:28:25 +0100

    Refactor 404 component to simplify code

--- src/components/404.jsx
+++ src/components/404.jsx
@@ -1,16 +1,12 @@
 import React from 'react';
 import './404.css';
 
-const NotFoundPage = () => {
-    return (
-        <div className="not-found-container">
-            <h1 className="not-found-title">404</h1>
-            <p className="not-found-message">Oops! The page you are looking for does not exist.</p>
-            <img src="/images/404-image.png" alt="404 Error" className="not-found-image" />
-            <a href="/" className="not-found-link">Go back to homepage</a>
-            {/* Images not configured yet */}
-        </div>
-    );
-};
 
-export default NotFoundPage;    
+const NotFound = () => (
+    <div className="not-found">
+        <h1>404</h1>
+        <h2>Page not found!</h2>
+    </div>
+);
+export default NotFound;
+
